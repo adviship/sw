d324a46e8b83c749131e44642829b639888dd8f1
adviship <adityaprakash93762@gmail.com>
2025-11-03 00:38:44 +0530

    Added repository

--- src/main/model/Sweet.java
+++ src/main/model/Sweet.java
@@ -1,3 +1,23 @@
-public class Sweet {
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Data
+@Table(name = "sweets")
+
+
+public class sweet {
+    @ID
+    private string id;
+    private string name;
+    private string type;
+    private double price;
+    private int quantity;
+    
     
 }
